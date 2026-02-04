21f57f5704c3bc895393819974e72428e61f4fd4
Mira Saini <mirasaini1005@icloud.com>
2026-02-03 18:26:13 -0800

    changed club request page to match new UI design

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@400;600;700&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
