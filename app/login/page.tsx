f6b2fe6c38dcc761c66b9438f24f4c2ec20516cb
davmantor <98769095+davmantor@users.noreply.github.com>
2026-09-22 01:03:23 -0700

    Update Google login button label to 'Sign in'

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -173,7 +173,7 @@ export default function LoginPage() {
                   d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z"
                 />
               </svg>
-              <span>Sign in with Google</span>
+              <span>Sign in</span>
             </button>
 
             {/* Dev Mode Simulated Login Options: Only in development */}
